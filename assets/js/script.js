fa5864c51dbcc89217043642d9f71fbb00aebb83
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-12-06 10:06:37 -0300

    adicionando acessibilidade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
-const btnShow = document.querySelector('.btn-menu');
+const btnMenu = document.querySelector('.botao-menu');
 const hamburguer = document.querySelector('.hamburguer');
 const menu = document.querySelector('.lista-links');
 
-btnShow.addEventListener('click', () => {
-    hamburguer.classList.toggle('ativado');
+// FUNCÃO PARA QUANDO MUDAR DE TELA O MENU FECHAR
+
+// FUNCÃO ABRIR E FECHAR MENU
+function menuShow(event) {
+
+    // MENU
     menu.classList.toggle('ativado');
-})
+
+    // ÍCONE MENU
+    hamburguer.classList.toggle('ativado');
+
+    // ACESSIBILIDADE
+    const ativado = menu.classList.contains('ativado');
+
+    if (ativado) {
+
+        event.currentTarget.setAttribute('aria-label', 'botão fechar menu');
+
+    } else {
+        
+        event.currentTarget.setAttribute('aria-label', 'botão abrir menu');
+
+    }
+
+    event.currentTarget.setAttribute('aria-expanded', ativado);
+
+}
+
+btnMenu.addEventListener('click', menuShow);
+btnMenu.addEventListener('touchstart', menuShow);
